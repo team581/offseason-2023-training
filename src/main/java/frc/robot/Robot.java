3d4fd216b29bb082bf803ccf0508a299cd22787a
Jonah Snider <jonah@jonahsnider.com>
2023-07-25 02:32:31 -0700

    Add arcade drive
    Signed-off-by: Jonah Snider <jonah@jonahsnider.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -6,4 +6,19 @@ package frc.robot;
 
 import org.littletonrobotics.junction.LoggedRobot;
 
-public class Robot extends LoggedRobot {}
+public class Robot extends LoggedRobot {
+  private TalonSRX driveRightAMotor = new TalonSRX(15);
+  private TalonSRX driveRightBMotor = new TalonSRX(8);
+  private TalonSRX driveLeftAMotor = new TalonSRX(14);
+  private TalonSRX driveLeftBMotor = new TalonSRX(2);
+  private XboxController xbox = new XboxController(0);
+
+  @Override
+  public void teleopPeriodic() {
+    double leftY = -xbox.getLeftY();
+    double rightY = -xbox.getRightY();
+
+    driveLeftAMotor.set(ControlMode.PercentOutput, leftY);
+    driveRightAMotor.set(ControlMode.PercentOutput, rightY);
+  }
+}
