671ff979e1e3b566fe30b89e3f55b6ff0b01638e
Owen <117956892+Owen756@users.noreply.github.com>
2023-07-25 17:59:20 -0700

    real one

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import org.littletonrobotics.junction.LoggedRobot;
@@ -9,36 +5,35 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj.XboxController;
 
-public class Robot extends LoggedRobot {
-  private TalonSRX frontLeft = new TalonSRX(2);
-  private TalonSRX rearLeft = new TalonSRX(8);
-  private TalonSRX rearRight = new TalonSRX(14);
-  private TalonSRX frontRight = new TalonSRX(15);
-  private XboxController xbox = new XboxController(0);
+    public class Robot extends LoggedRobot {
+        private TalonSRX frontLeft = new TalonSRX(2);
+        private TalonSRX rearLeft = new TalonSRX(8);
+        private TalonSRX rearRight = new TalonSRX(14);
+        private TalonSRX frontRight = new TalonSRX(15);
+        private XboxController xbox = new XboxController(0);
+        @Override
+        public void robotInit() {
+            rearRight.setInverted(true);
+            frontRight.setInverted(true);
+            frontRight.enableCurrentLimit(true);
+            frontLeft.enableCurrentLimit(true);
+            rearRight.enableCurrentLimit(true);
+            rearLeft.enableCurrentLimit(true);
+            frontRight.configPeakCurrentLimit(50);
+            frontLeft.configPeakCurrentLimit(50);
+            rearRight.configPeakCurrentLimit(50);
+            rearLeft.configPeakCurrentLimit(50);
+        }
 
-  @Override
-  public void robotInit() {
-    rearRight.setInverted(true);
-    frontRight.setInverted(true);
-    frontRight.enableCurrentLimit(true);
-    frontLeft.enableCurrentLimit(true);
-    rearRight.enableCurrentLimit(true);
-    rearLeft.enableCurrentLimit(true);
-    frontRight.configPeakCurrentLimit(50);
-    frontLeft.configPeakCurrentLimit(50);
-    rearRight.configPeakCurrentLimit(50);
-    rearLeft.configPeakCurrentLimit(50);
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    double rightX = xbox.getRightX();
-    double leftY = -xbox.getLeftY();
-    double leftSpeed = leftY + rightX;
-    double rightSpeed = leftY - rightX;
-    frontLeft.set(ControlMode.PercentOutput, leftSpeed);
-    rearLeft.set(ControlMode.PercentOutput, leftSpeed);
-    frontRight.set(ControlMode.PercentOutput, rightSpeed);
-    rearRight.set(ControlMode.PercentOutput, rightSpeed);
-  }
-}
+        @Override
+        public void teleopPeriodic() {
+            double rightX = xbox.getRightX();
+            double leftY = -xbox.getLeftY();
+            double leftSpeed = leftY + rightX;
+            double rightSpeed = leftY - rightX;
+            frontLeft.set(ControlMode.PercentOutput, leftSpeed);
+            rearLeft.set(ControlMode.PercentOutput, leftSpeed);
+            frontRight.set(ControlMode.PercentOutput, rightSpeed);
+            rearRight.set(ControlMode.PercentOutput, rightSpeed);
+        }
+    }
