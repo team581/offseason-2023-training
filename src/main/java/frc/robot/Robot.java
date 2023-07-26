a590a64cc33bee52a6c1f641fa839ce9a4aad629
Chris <117790940+JelloSmello@users.noreply.github.com>
2023-07-25 17:38:39 -0700

    Add arcade drive

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,29 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import org.littletonrobotics.junction.LoggedRobot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-public class Robot extends LoggedRobot {}
+public class Robot extends LoggedRobot {
+  private TalonSRX driveFrontRight = new TalonSRX(15);
+  private TalonSRX driveRearRight = new TalonSRX(14);
+  private TalonSRX driveFrontLeft = new TalonSRX(2);
+  private TalonSRX driveRearLeft = new TalonSRX(8);
+  private XboxController xbox = new XboxController(0);
+
+   @Override
+   public void teleopPeriodic() {
+    double rightY = xbox.getRightY();
+    double leftY = -xbox.getLeftY();
+    double rightX = xbox.getRightX();
+    double leftSpeed = xbox.getRightX() + xbox.getLeftY();
+    double rightSpeed = xbox.getLeftY() - xbox.getRightX();
+
+    driveFrontRight.set(ControlMode.PercentOutput, rightSpeed);
+    driveFrontLeft.set(ControlMode.PercentOutput, leftSpeed);
+    driveRearLeft.set(ControlMode.PercentOutput, leftSpeed);
+    driveRearRight.set(ControlMode.PercentOutput, rightSpeed);
+}
+}
