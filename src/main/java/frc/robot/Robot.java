8d93d6f21ac0e137820fd547ea4e3cb2150729d4
Simon <87281874+simonp17@users.noreply.github.com>
2023-07-25 17:38:36 -0700

    Add arcade drive with inverted left side

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,5 +5,37 @@
 package frc.robot;
 
 import org.littletonrobotics.junction.LoggedRobot;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.XboxController;
 
-public class Robot extends LoggedRobot {}
+public class Robot extends LoggedRobot {
+  private TalonSRX driveRightAMotor = new TalonSRX(15);
+  private TalonSRX driveRightBMotor = new TalonSRX(14);
+  private TalonSRX driveLeftAMotor = new TalonSRX(2);
+  private TalonSRX driveLeftBMotor = new TalonSRX(8);
+  private XboxController xbox = new XboxController(0);
+
+  @Override
+  public void teleopPeriodic() {
+    double rightY = -xbox.getRightY();
+    //double leftY = -xbox.getLeftY();
+    double leftX = xbox.getLeftX();
+
+    double leftSpeed = rightY + leftX;
+    double rightSpeed = rightY - leftX;
+
+    driveRightAMotor.set(ControlMode.PercentOutput, rightSpeed);
+    driveRightBMotor.set(ControlMode.PercentOutput, rightSpeed);
+    driveLeftAMotor.set(ControlMode.PercentOutput, leftSpeed);
+    driveLeftBMotor.set(ControlMode.PercentOutput, leftSpeed);
+
+  }
+
+  @Override
+  public void robotInit() {
+    driveLeftAMotor.setInverted(true);
+    driveLeftBMotor.setInverted(true);
+  }
+
+}
