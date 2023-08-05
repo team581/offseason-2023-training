2d1da775690c4b43184bc73a34443a41b5abe1e9
Jonah Snider <jonah@jonahsnider.com>
2023-08-05 01:21:12 -0700

    Add bang bang control of wrist

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,10 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.util.scheduling.LifecycleSubsystemManager;
 import org.littletonrobotics.junction.LoggedRobot;
@@ -11,6 +15,13 @@ import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 
 public class Robot extends LoggedRobot {
+  private XboxController xboxController = new XboxController(0);
+  private double wristTolerance = 1;
+  public double goalAngle = 0;
+  public boolean wristDisabled = true;
+  private TalonFX wristMotor = new TalonFX(16);
+  public VoltageOut wristVoltage = new VoltageOut(0);
+
   public Robot() {
     Logger.getInstance().addDataReceiver(new NT4Publisher());
     Logger.getInstance().start();
@@ -20,5 +31,39 @@ public class Robot extends LoggedRobot {
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (xboxController.getAButton()) {
+      wristDisabled = true;
+    } else if (xboxController.getBButton()) {
+      wristMotor.setRotorPosition(0);
+      goalAngle = 0;
+      wristDisabled = false;
+    } else if (xboxController.getXButton()) {
+      goalAngle = 10;
+      wristDisabled = false;
+    } else if (xboxController.getYButton()) {
+      goalAngle = 50;
+      wristDisabled = false;
+    }
+
+    if (wristDisabled) {
+      wristMotor.disable();
+    } else {
+      if (getWristAngle() > goalAngle - wristTolerance) {
+        wristMotor.setControl(wristVoltage.withOutput(-1));
+      } else if (getWristAngle() < goalAngle + wristTolerance) {
+        wristMotor.setControl(wristVoltage.withOutput(1));
+      } else {
+        wristMotor.setControl(wristVoltage.withOutput(0));
+      }
+    }
+  }
+
+  private double getWristAngle() {
+    StatusSignal<Double> wristMotorRotations = wristMotor.getRotorPosition();
+    return wristMotorRotations.getValue() / 50.0 * 360.0;
   }
 }
