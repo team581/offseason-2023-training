bd4f02a808e6f54e4b9ae1341dd5b356850aca6a
Aidan <>
2023-08-05 09:13:38 -0700

    arm control

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,14 +4,45 @@
 
 package frc.robot;
 
+import com.ctre.phoenix6.StatusSignal;
+import com.ctre.phoenix6.controls.VoltageOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.XboxController;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 
 public class Robot extends LoggedRobot {
+  XboxController controller = new XboxController(0);
+  TalonFX armMotor = new TalonFX(17);
+  VoltageOut armVoltage = new VoltageOut(0);
+
+
   public Robot() {
     Logger.getInstance().addDataReceiver(new NT4Publisher());
 
     Logger.getInstance().start();
+  }
+
+  @Override
+  public void teleopPeriodic() {
+  StatusSignal<Double> armRotor = armMotor.getRotorPosition();
+  double armRotations = armRotor.getValue();
+  boolean aPressed = controller.getAButtonPressed();
+  boolean bPressed = controller.getBButtonPressed();
+  boolean xPressed = controller.getXButtonPressed();
+  boolean yPressed = controller.getYButtonPressed();
+    Logger.getInstance().recordOutput("Arm/Position", armRotations);
+    double armDegrees = armRotations / 360;
+    if (aPressed) {
+      armMotor.setControl(armVoltage.withOutput(0.0));
+    }
+    if (bPressed) {
+      armRotations = 0;
+    }
+    if (xPressed) {
+
+    }
+    if (yPressed) {}
   }
 }
