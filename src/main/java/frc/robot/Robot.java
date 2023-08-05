ff06b6cd22eaf32fb39bec20bc26e2c7103c88d0
ryanknj5 <140574269+ryanknj5@users.noreply.github.com>
2023-08-05 09:21:10 -0700

    Arm control by ryan n

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,66 +4,80 @@
 
 package frc.robot;
 
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.util.scheduling.LifecycleSubsystemManager;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 
 public class Robot extends LoggedRobot {
-  private XboxController xboxController = new XboxController(0);
-  private double wristTolerance = 1;
-  public double goalAngle = 0;
-  public boolean wristDisabled = true;
-  private TalonFX wristMotor = new TalonFX(16);
-  public VoltageOut wristVoltage = new VoltageOut(0);
+    public Robot() {
+        Logger.getInstance().addDataReceiver(new NT4Publisher());
 
-  public Robot() {
-    Logger.getInstance().addDataReceiver(new NT4Publisher());
-    Logger.getInstance().start();
-    LifecycleSubsystemManager.getInstance().ready();
-  }
-
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
-
-  @Override
-  public void teleopPeriodic() {
-    if (xboxController.getAButton()) {
-      wristDisabled = true;
-    } else if (xboxController.getBButton()) {
-      wristMotor.setRotorPosition(0);
-      goalAngle = 0;
-      wristDisabled = false;
-    } else if (xboxController.getXButton()) {
-      goalAngle = 10;
-      wristDisabled = false;
-    } else if (xboxController.getYButton()) {
-      goalAngle = 50;
-      wristDisabled = false;
+        Logger.getInstance().start();
     }
 
-    if (wristDisabled) {
-      wristMotor.disable();
-    } else {
-      if (getWristAngle() > goalAngle - wristTolerance) {
-        wristMotor.setControl(wristVoltage.withOutput(-1));
-      } else if (getWristAngle() < goalAngle + wristTolerance) {
-        wristMotor.setControl(wristVoltage.withOutput(1));
-      } else {
-        wristMotor.setControl(wristVoltage.withOutput(0));
-      }
-    }
-  }
+    @Override
+    public void teleopPeriodic() {
+        // do something
+        private TalonFX armMotor = new TalonFX(17);
+        private XboxController controller = new XboxController(0);
+        private VoltageOut armVoltage = new VoltageOut(0);
 
-  private double getWristAngle() {
-    StatusSignal<Double> wristMotorRotations = wristMotor.getRotorPosition();
-    return wristMotorRotations.getValue() / 50.0 * 360.0;
-  }
-}
+        boolean aPressed = controller.getAButtonPressed();
+        boolean bPressed = controller.getBButtonPressed();
+        boolean xPressed = controller.getXButtonPressed();
+        boolean yPressed = controller.getYButtonPressed();
+        StatusSignal<Double> armRotor = armMotor.getRotorPosition();
+        double armRotations = armRotor.getValue();
+        double armAngle = armRotations*45/256;
+
+            if (aPressed==true){
+                armMotor.setControl(armVoltage.withOutput(0.0));
+            }
+            if(bPressed==true){
+                while (armAngle!=0){
+                    if(armAngle > 0){
+                    armMotor.setControl(armVoltage.withOutput(-0.2));
+                    }
+                        else if (armAngle < 0){
+                            armMotor.setControl(armVoltage.withOutput(0.2));
+                        }
+                            else{
+                                armMotor.setControl(armVoltage.withOutput(0.0));
+                            }
+                }
+
+
+            }
+            if(xPressed == true){
+                while (armAngle!=10){
+                    if(armAngle > 10){
+                    armMotor.setControl(armVoltage.withOutput(-0.2));
+                    }
+                        else if (armAngle < 10){
+                            armMotor.setControl(armVoltage.withOutput(0.2));
+                        }
+                            else{
+                                armMotor.setControl(armVoltage.withOutput(0.0));
+                            }
+                }
+
+            }
+            if(yPressed == true){
+                while (armAngle!=50){
+                    if(armAngle > 50){
+                    armMotor.setControl(armVoltage.withOutput(-0.2));
+                }
+                        else if (armAngle < 50){
+                            armMotor.setControl(armVoltage.withOutput(0.2));
+                        }
+                            else{
+                                armMotor.setControl(armVoltage.withOutput(0.0));
+                            }
+                }
+
+            }
+                }
+            }
